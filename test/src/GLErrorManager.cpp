26a8c70ca4d36f68c37099c2516e1806420f9a2e
C0de871 <muhammadaydi3@gmail.com>
2024-11-15 14:10:28 +0300

    add move error opengl error handling from Renderer to GlErrorManager and abstract Drawing functions to Renderer

---
+++ test/src/GLErrorManager.cpp
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <iostream>
+
+//clear all previous errors:
+void GlClearError() {
+	while (glGetError() != GL_NO_ERROR);
+}
+
+//log the error message to the consol 
+bool GlLogCall(const char* function, const char* file, int line) {
+	while (GLenum error = glGetError()) {
+		std::cout << "[OpenGl Error code]: (" << error << ") \n" << "[the function that cause the error]: " << function << "\n[The path of the file that cause the error]: " << file << "\n[The line]: " << line << std::endl;
+		return false;
+	}
+	return true;
+}
--- test/src/Renderer.h
+++ test/src/Renderer.h
@@ -1,16 +1,10 @@
 #pragma once
+#include "VertexArray.h";
+#include "IndexBuffer.h";
+#include "Shader.h";
 
-#include <glad/glad.h>
-
-#define ASSERT(x) if (!(x)) __debugbreak();
-
-//error handling for opengl errors that can't be seen by the compiler:
-#define GlCall(x) GlClearError();\
-	x;\
-	ASSERT(GlLogCall(#x,__FILE__,__LINE__))
-
-//clear all previous errors:
-void GlClearError();
-
-//log the error message to the consol 
-bool GlLogCall(const char* function, const char* file, int line);
+class Renderer {
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
